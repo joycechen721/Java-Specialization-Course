13d0574032b849dc1ef227c58e591bc13de7f5c6
Joyce Chen <joyce.chen721@gmail.com>
2022-06-21 00:12:44 -0700

    too lazy to finish assignment...

---
+++ src/course_3/CaesarCipherStuff/CaesarCipher.java
@@ -0,0 +1,12 @@
+package course_3.CaesarCipherStuff;
+
+public class CaesarCipher {
+    public static void main(String [] args){
+
+    }
+
+    //assuming all alphabetic characters are uppercase letters
+    public String encrypt(String input, int key){
+
+    }
+}
--- src/course_3/CaesarCipherStuff/WordPlay.java
+++ src/course_3/CaesarCipherStuff/WordPlay.java
@@ -2,6 +2,35 @@ package course_3.CaesarCipherStuff;
 
 public class WordPlay {
     public static void main (String [] args){
+        System.out.println(replaceVowels("method", 'x'));
+        System.out.println(emphasize("dna ctgaaactga", 'a') );
+    }
 
+    public static boolean isVowel (char ch){
+        char lower = Character.toLowerCase(ch);
+        return lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u';
+    }
+
+    public static String replaceVowels (String phrase, char ch){
+        StringBuilder replaced = new StringBuilder();
+        for (int i = 0; i < phrase.length(); i++) {
+            char currChar = phrase.charAt(i);
+            if (isVowel(currChar)) replaced.append(ch);
+            else replaced.append(currChar);
+        }
+        return replaced.toString();
+    }
+
+    public static String emphasize (String phrase, char ch){
+        StringBuilder replaced = new StringBuilder();
+        for (int i = 0; i < phrase.length(); i++) {
+            char currChar = phrase.charAt(i);
+            if (Character.toLowerCase(currChar) == ch){
+                if (i % 2 == 0) replaced.append('*');
+                else replaced.append('+');
+            }
+            else replaced.append(currChar);
+        }
+        return replaced.toString();
     }
 }
